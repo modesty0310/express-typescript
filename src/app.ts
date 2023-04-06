e634a69fde583950fe6ca6347f3f2eb581f16b8b
modesty0310 <rlatprua4@gmail.com>
2023-04-06 15:19:30 +0900

    pino util 생성

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import config from 'config'
+import logger from './utils/logger'
 
-const port = config.get<number>('port')
+const port = config.get<number>('port');
 
 const app = express();
 
 app.listen(port, async () => {
-    console.log('app start');
+    logger.info('app start');
     
 })
